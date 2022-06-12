bc483d5ca2666d4db22bb71da10456581a948c34
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-06-12 22:49:42 +0300

    state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project';
+  members: string[] = [];
+  newMemberName = '';
+
+  addMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
